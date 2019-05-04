06aa5ad5f2b4e9de75415126bdf1083c05353d2f
PajarKharisma <pajarkharisma@hotmail.com>
2019-05-04 19:34:33 +0700

    remove exe file in 19 - Problem Solving

--- 19 - Problem Solving/Main.cpp
+++ 19 - Problem Solving/Main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <conio.h>
 #include <vector>
@@ -20,27 +19,4 @@ int main(){
     }
     cout << "Selisih maximal adalah : " << max;
     getch();
-=======
-#include <iostream>
-#include <conio.h>
-#include <vector>
-using namespace std;
-
-int main(){
-    int idr[] = {14086, 14136, 14137, 14224, 14227, 14221, 14216, 14229, 14253};
-    int size = sizeof(idr)/sizeof(idr[0]);
-    vector<int> diffList;
-    for(int i=size-1; i>0; i--){
-        int diff = abs(idr[i] - idr[i-1]);
-        diffList.push_back(diff);
-    }
-    
-    int max = 0;
-    for(auto i:diffList){
-        if(i > max)
-            max = i;
-    }
-    cout << "Selisih maximal adalah : " << max;
-    getch();
->>>>>>> 1f845297ad6e07ee7196038a5edd1fb7b7546d36
 }
