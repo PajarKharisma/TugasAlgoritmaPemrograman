cced07c8bb2ef369a50664642c54e64dee777df5
PajarKharisma <pajarkharisma@hotmail.com>
2019-05-06 13:54:45 +0700

    add 20 - Latihan Responsi

---
+++ 20 - Latihan Responsi/Code1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+    int peserta[] = {367, 475, 389, 274, 485, 492, 263, 394, 476, 358, 482, 249, 377, 472, 453, 282, 375, 494, 286, 468};
+    
+    getch();
+}
