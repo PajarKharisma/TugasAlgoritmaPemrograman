e7901f1d58b8210acfc1a4f837c71bc15aefb5f6
PajarKharisma <pajarkharisma@hotmail.com>
2019-05-06 20:11:57 +0700

    20 - Latihan Responsi v1.0

--- 20 - Latihan Responsi/Code2.cpp
+++ 20 - Latihan Responsi/Code2.cpp
@@ -4,14 +4,60 @@
 #include <cstring>
 using namespace std;
 
-int main(){
-    string a = "senin";
-    char nama[a.size()+1];
-    strcpy(nama, a.c_str());
-    int sum = 0;
-    for(int i=0; i<a.size(); i++){
-        sum += (int)nama[i];
+struct Hash {
+    int getBigestPrime(int val){
+        bool isPrime = false;
+        int bigestPrime = -1;
+        while(!isPrime && val >= 0){
+            int cek = 0;
+            for(int i=1; i<=val; i++){
+                if(val % i == 0){
+                    cek++;
+                }
+            }
+            if(cek <= 2){
+                isPrime = true;
+                bigestPrime = val;
+            }
+            val--;
+        }
+        return bigestPrime;
     }
-    cout << sum << endl;
+
+    int getStringValue(string s){
+        char c[s.size()+1];
+        strcpy(c, s.c_str());
+        int sum = 0;
+        for(int i=0; i<s.size(); i++){
+            sum += (int)c[i];
+        }
+        return sum;
+    }
+
+    void hashing(string data, string kamus[]){
+        int p = getBigestPrime(100);
+        int val = getStringValue(data);
+        int index = val % p;
+        while(kamus[index] != ""){
+            index++;
+        }
+        kamus[index] = data;
+    }
+};
+
+
+int main(){
+    Hash hash;
+    string kamus[100];
+    string data[] = {"hari", "senin", "belajar", "giat", "agar", "kelak", "baik", "hasil", "tidak", "ada", "sesal", "dan", "besok", "tetap", "semangat", "kerja", "juang", "demi", "masa", "depan", "lebih", "cerah"};
+    int n = sizeof(data)/sizeof(data[0]);
+    for(int i=0; i<n; i++){
+        hash.hashing(data[i], kamus);
+    }
+
+    for(int i=0; i<100; i++){
+        if(kamus[i] != "")
+            cout << i << "-" << kamus[i] << endl;
+    }
     getch();
 }
