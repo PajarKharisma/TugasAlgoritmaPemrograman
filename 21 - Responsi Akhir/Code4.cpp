71acf3379e5347b83780942cef0cf085a6b612ed
PajarKharisma <pajarkharisma@hotmail.com>
2019-05-10 08:22:37 +0700

    sudah no 1 2 4

---
+++ 21 - Responsi Akhir/Code4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+//nama, nomor induk kependudukan, tanggal lahir, jenis kelamin, alamat rumah, email, dan nomor hp
+
+struct CalonPegawai {
+    string name;
+    string nik;
+    string tanggalLahir;
+    char jenisKelamin;
+    string alamat;
+    string email;
+    string noHP;
+};
+
+
+int main(){
+
+}
